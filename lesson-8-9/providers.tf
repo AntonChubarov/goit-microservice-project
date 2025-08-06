dde21b1bd19b430144f0b05fca68d9326d1c54e8
AntonChubarov <achlp600@gmail.com>
2025-08-06 07:38:51 -0400

    Added changes for Jenkins and Argo CD (12)

--- lesson-8-9/providers.tf
+++ lesson-8-9/providers.tf
@@ -36,7 +36,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
-  load_config_file       = false
 }
 
 provider "helm" {
@@ -56,7 +55,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
-  load_config_file       = false
 }
 
 provider "helm" {
